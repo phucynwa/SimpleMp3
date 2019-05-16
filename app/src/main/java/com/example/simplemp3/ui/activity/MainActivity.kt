da108624d06fe61a49460d05069b49af79bd9a23
PhucSSK <lhphuc.ynwa@gmail.com>
2019-05-16 08:42:16 +0700

    init project

---
+++ app/src/main/java/com/example/simplemp3/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.simplemp3.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.simplemp3.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
